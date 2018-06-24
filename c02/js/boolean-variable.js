51a4c8d268dc0cae1eca9fc1de19c44abc25a1f7
ewa1siwek <ew.siwek@gmail.com>
2018-06-25 01:26:22 +0200

    boolean-variable

--- c02/js/boolean-variable.js
+++ c02/js/boolean-variable.js
@@ -1,3 +1,15 @@
+var inStock;
+var shipping;
+
+inStock = true;
+shipping = false;
+
+var elStock = document.getElementById('stock');
+elStock.classList = inStock;
+var elShip = document.getElementById('shipping');
+elShip.classList = shipping;
+
+/*
 // Create variables and assign their values
 var inStock;
 var shipping;
@@ -12,4 +24,4 @@ elStock.className = inStock;
 // Get the element that has an id of shipping
 var elShip = document.getElementById('shipping');
 // Set class name with value of shipping variable
-elShip.className = shipping;
+elShip.className = shipping;*/

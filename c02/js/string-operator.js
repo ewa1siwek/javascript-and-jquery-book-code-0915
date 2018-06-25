9bafcad442105b113205b0f9931b2df1c76d45a6
ewa1siwek <ew.siwek@gmail.com>
2018-06-26 00:11:42 +0200

    string-operator

--- c02/js/string-operator.js
+++ c02/js/string-operator.js
@@ -1,10 +1,20 @@
+var greeting = 'Cześć, ';
+var name = 'Paweł'
+
+var welcomeMessage = greeting + name + '!';
+
+var el = document.getElementById('greeting');
+
+el.textContent = welcomeMessage;
+
+/*
 // Store the greeting in a variable
 var greeting = 'Howdy ';
 
 // Store the users name in a variable
 var name = 'Molly';
 
-/* Create the welcome message by concatenating the strings in the two variables */
+/!* Create the welcome message by concatenating the strings in the two variables *!/
 var welcomeMessage = greeting + name + '!';
 
 // Get the element that has an id of greeting
@@ -13,8 +23,8 @@ var el = document.getElementById('greeting');
 // Replace the content of this element with the personal message
 el.textContent = welcomeMessage;
 
-/* 
+/!*
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on line 14, but note the security issues on p228-231
 el.innerHTML = welcomeMessage;
-*/
+*!/*/

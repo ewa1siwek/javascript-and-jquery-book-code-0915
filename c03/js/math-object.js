94a56045951e872b08a6eb035ac0696c347e4bda
ewa1siwek <ew.siwek@gmail.com>
2018-06-27 00:00:36 +0200

    c3_date-object

--- c03/js/math-object.js
+++ c03/js/math-object.js
@@ -1,7 +1,7 @@
 var randomNum = Math.floor((Math.random() * 10) + 1);
 
 var el = document.getElementById('info');
-el.innerHTML = '<h2>Random number 1 - 10:</h2><p>' + randomNum + '</p>';
+el.innerHTML = '<h2>Random number 1 - 10:</h2><p>' + randomNum  + '</p>';
 
 /*
 // Create a variable to hold a random number between 1 and 10

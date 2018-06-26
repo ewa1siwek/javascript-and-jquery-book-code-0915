512947fd3993a1fdf0a5bfbd2dec8c32d677c313
ewa1siwek <ew.siwek@gmail.com>
2018-06-27 01:51:32 +0200

    c4_logical-or-not

--- c04/js/logical-or-logical-not.js
+++ c04/js/logical-or-logical-not.js
@@ -1,3 +1,17 @@
+var score1 = 19;
+var score2 = 89;
+
+var pass1 = 16;
+var pass2 = 30;
+
+var minPass = (score1 >= pass1) || (score2 >= pass2);
+
+var msg = 'Resit required: ' + !minPass;
+
+var el = document.getElementById('answer');
+el.innerHTML = msg;
+
+/*
 var score1 = 8;   // Round 1 score
 var score2 = 8;   // Round 2 score
 var pass1 = 6;    // Round 1 pass mark
@@ -11,4 +25,4 @@ var msg = 'Resit required: ' + !minPass;
 
 // Write the message into the page
 var el = document.getElementById('answer');
-el.innerHTML = msg;
+el.innerHTML = msg;*/

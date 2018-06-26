1b15699daf483a36a00600ba6b134e3beaab12f4
ewa1siwek <ew.siwek@gmail.com>
2018-06-26 16:01:36 +0200

    c3_object-constructor

--- c03/js/object-constructor.js
+++ c03/js/object-constructor.js
@@ -1,3 +1,19 @@
+var hotel = new Object();
+
+hotel.name = 'Park';
+hotel.rooms = 120;
+hotel.booked = 79;
+hotel.checkAvailability = function () {
+    return this.rooms - this.booked;
+};
+
+var elName = document.getElementById('hotelName');
+elName.textContent = hotel.name;
+
+var elRooms = document.getElementById('rooms');
+elRooms.textContent = hotel.checkAvailability();
+
+/*
 // Set up the object
 var hotel = new Object();
 
@@ -14,7 +30,7 @@ elName.textContent = hotel.name;                   // Update HTML with property 
 var elRooms = document.getElementById('rooms');    // Get element
 elRooms.textContent = hotel.checkAvailability();   // Update HTML with result of method
 
-/* 
+/!*
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on lines 12 and 15, but note the security issues on p228-231
-*/
+*!/*/

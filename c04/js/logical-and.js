c41dbc180fe8157a9ad3ced5420fc1a40defe6f5
ewa1siwek <ew.siwek@gmail.com>
2018-06-27 01:39:28 +0200

    c4_logical-and

--- c04/js/logical-and.js
+++ c04/js/logical-and.js
@@ -1,3 +1,16 @@
+var score1 = 80;
+var score2 = 90;
+
+var pass1 = 70;
+var pass2 = 30;
+
+var passBoth = (score1 >= pass1) && (score2 >= pass2);
+var msg = 'Both rounds passed: ' + passBoth;
+
+var el = document.getElementById('answer');
+el.innerHTML = msg;
+
+/*
 var score1 = 8;   // Round 1 score
 var score2 = 8;   // Round 2 score
 var pass1 = 6;    // Round 1 pass mark
@@ -11,4 +24,4 @@ var msg = 'Both rounds passed: ' + passBoth;
 
 // Write the message into the page
 var el = document.getElementById('answer');
-el.innerHTML = msg;
+el.innerHTML = msg;*/

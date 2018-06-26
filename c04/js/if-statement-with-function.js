5fd1a4311e09a82ff158a77651166cf11b39e818
ewa1siwek <ew.siwek@gmail.com>
2018-06-27 01:59:50 +0200

    c4_if-statement-with-function

--- c04/js/if-statement-with-function.js
+++ c04/js/if-statement-with-function.js
@@ -1,3 +1,18 @@
+var score = 60;
+var msg = '';
+
+function congratulate() {
+    msg += 'Congratulations! ';
+}
+
+if (score > 30) {
+  congratulate();
+  msg += 'Proceed to the next round.';
+}
+
+var el = document.getElementById('answer');
+el.innerHTML = msg;
+/*
 var score = 75;    // Score
 var msg = '';      // Message
 
@@ -11,4 +26,4 @@ if (score >= 50) {  // If score is 50 or more
 }
 
 var el = document.getElementById('answer');
-el.innerHTML = msg;
+el.innerHTML = msg;*/

6d67b7a5fde2bfe6fc4bf93f9b12f85befe35a73
ewa1siwek <ew.siwek@gmail.com>
2018-06-26 16:25:46 +0200

    c3_multiple-object

--- c03/js/multiple-objects.js
+++ c03/js/multiple-objects.js
@@ -1,3 +1,26 @@
+function Hotel(name, rooms, booked) {
+    this.name = name;
+    this.rooms = rooms;
+    this.booked = booked;
+    this.checkAvailability = function () {
+        return this.rooms - this.booked;
+    };
+}
+
+var qHotel = new Hotel('Q', 39, 14);
+var parkHotel = new Hotel('Park', 140, 65);
+
+var detailsQ = qHotel.name + ' rooms: ';
+detailsQ += qHotel.checkAvailability();
+var elQHotel = document.getElementById('hotel1');
+elQHotel.textContent = detailsQ;
+
+var detailsPark = parkHotel.name + ' rooms: ';
+detailsPark += parkHotel.checkAvailability();
+var elParkHotel = document.getElementById('hotel2');
+elParkHotel.textContent = detailsPark;
+
+/*
 // Create the template for objects that are hotels
 function Hotel(name, rooms, booked) {
   this.name = name;
@@ -25,7 +48,7 @@ var details2 = parkHotel.name + ' rooms: ';
 var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
 
-/* 
+/!*
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on lines 21 and 26, but note the security issues on p228-231
-*/
+*!/*/

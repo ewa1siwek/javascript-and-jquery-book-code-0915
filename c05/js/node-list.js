690aee9db91dd29dd1320ac2a27a0562d5d0a906
ewa1siwek <ew.siwek@gmail.com>
2018-06-27 15:36:12 +0200

    c5_node-list

--- c05/js/node-list.js
+++ c05/js/node-list.js
@@ -1,8 +1,15 @@
-var hotItems = document.querySelectorAll('li.hot'); // Store NodeList in array
+var hotItems = document.querySelectorAll('li.hot');
+
+if (hotItems.length > 0) {
+  for (var i = 0; i < hotItems.length; i++) {
+    hotItems[i].className = 'cool';
+  }
+}
+/*var hotItems = document.querySelectorAll('li.hot'); // Store NodeList in array
 if (hotItems.length > 0) {                          // If it contains items
 
   for (var i = 0; i < hotItems.length; i++) {       // Loop through each item
     hotItems[i].className = 'cool';         // Change value of class attribute
   }
 
-}
+}*/

43368fa75b6c8b1d5c53cb9736f29bc638f71aa6
ewa1siwek <ew.siwek@gmail.com>
2018-06-27 02:13:51 +0200

    c4_switch-statement

--- c04/js/switch-statement.js
+++ c04/js/switch-statement.js
@@ -1,3 +1,25 @@
+var level = 2;
+var msg;
+
+switch (level) {
+    case 1:
+        msg = 'Good luck on thefirst test';
+        break;
+    case 2:
+        msg = 'Second of three - keep going!';
+        break;
+
+    case 3:
+        msg = 'Final round - keep going';
+        break;
+    default:
+        msg = 'Good luck!'
+        break;
+}
+
+var el = document.getElementById('answer');
+el.innerHTML = msg;
+/*
 var msg;        // Message
 var level = 2;  // Level
 
@@ -21,4 +43,4 @@ default:
 }
 
 var el = document.getElementById('answer');
-el.textContent = msg;
+el.textContent = msg;*/

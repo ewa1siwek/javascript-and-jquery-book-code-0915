fe9d38b5abb92d38f0f578f663f4069654f7a6b1
ewa1siwek <ew.siwek@gmail.com>
2018-06-27 17:26:14 +0200

    c5_set-attribute-className

--- c05/js/set-attribute.js
+++ c05/js/set-attribute.js
@@ -1,6 +1,12 @@
+var firstItem = document.getElementById('one');
+firstItem.className = 'complete';
+
+var fourthItem = document.getElementsByTagName('li')[3];
+fourthItem.setAttribute('class', 'cool');
+/*
 var firstItem = document.getElementById('one'); // Get the first item
 firstItem.className = 'complete';               // Change its class attribute
 
 var fourthItem = document.getElementsByTagName('li').item(3);// Get fourth item
 // NOTE: The following line should say fourthItem (not el2)
-fourthItem.setAttribute('class', 'cool');       // Add an attribute to it
+fourthItem.setAttribute('class', 'cool');       // Add an attribute to it*/

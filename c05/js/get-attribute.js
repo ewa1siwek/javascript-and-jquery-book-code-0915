0e0fa0be72192d9b4d28500569d72671455d2bd9
ewa1siwek <ew.siwek@gmail.com>
2018-06-27 17:21:06 +0200

    c5_get-attribute

--- c05/js/get-attribute.js
+++ c05/js/get-attribute.js
@@ -1,9 +1,19 @@
-var firstItem = document.getElementById('one'); // Get first list item 
-if (firstItem.hasAttribute('class')) {          // If it has class attribute
-  var attr = firstItem.getAttribute('class');   // Get the attribute
+var firstItem = document.getElementById('one');
 
-  // Add the value of the attribute after the list
-  var el = document.getElementById('scriptResults');
+if (firstItem.hasAttribute('class')) {
+  var attr = firstItem.getAttribute('class');
+
+    var el = document.getElementById('scriptResults');
   el.innerHTML = '<p>The first item has a class name: ' + attr + '</p>';
-
 }
+
+//
+// var firstItem = document.getElementById('one'); // Get first list item
+// if (firstItem.hasAttribute('class')) {          // If it has class attribute
+//   var attr = firstItem.getAttribute('class');   // Get the attribute
+//
+//   // Add the value of the attribute after the list
+//   var el = document.getElementById('scriptResults');
+//   el.innerHTML = '<p>The first item has a class name: ' + attr + '</p>';
+//
+// }

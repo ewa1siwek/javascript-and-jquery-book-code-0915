4efced688de71cc49736d9e57638754f4e3669dc
ewa1siwek <ew.siwek@gmail.com>
2018-06-28 17:02:14 +0200

    c6_event-listener-with-event-object1

--- c06/js/event-listener-with-event-object.js
+++ c06/js/event-listener-with-event-object.js
@@ -1,53 +1,27 @@
-function checkLength(e, minLength) {
-    var el, elMsg;
-    if (!e) {
-      e = window.event;
-    }
-    el = e.target || e.srcElement;
-    elMsg = el.nextSibling;
-
-    if (el.value.length < minLength) {
-      elMsg.innerHTML = 'Ilość znaków przynajmniej ' + minLength;
-    } else {
-      elMsg.innerHTML = '';
-    }
-}
-
-var elUsername = document.getElementById('username');
-if (elUsername.addEventListener) {
-  elUsername.addEventListener('blur', function(e) {
-      checkLength(e, 6)
-  }, false)
-} else {
-  elUsername.attachEvent('onblur', function (e) {
-      checkLength(e, 6)
-  })
-}
-/*
 function checkLength(e, minLength) {         // Declare function
-  var el, elMsg;                             // Declare variables
-  if (!e) {                                  // If event object doesn't exist
-    e = window.event;                        // Use IE fallback
-  }
-  el = e.target || e.srcElement;             // Get target of event
-  elMsg = el.nextSibling;                    // Get its next sibling
+    var el, elMsg;                             // Declare variables
+    if (!e) {                                  // If event object doesn't exist
+        e = window.event;                        // Use IE fallback
+    }
+    el = e.target || e.srcElement;             // Get target of event
+    elMsg = el.nextSibling;                    // Get its next sibling
 
-  if (el.value.length < minLength) {         // If length is too short set msg
-    elMsg.innerHTML = 'Username must be ' + minLength + ' characters or more';
-  } else {                                   // Otherwise
-    elMsg.innerHTML = '';                    // Clear message
-  }
+    if (el.value.length < minLength) {         // If length is too short set msg
+        elMsg.innerHTML = 'Username must be ' + minLength + ' characters or more';
+    } else {                                   // Otherwise
+        elMsg.innerHTML = '';                    // Clear message
+    }
 }
 
 var elUsername = document.getElementById('username');// Get username input
 if (elUsername.addEventListener) {           // If event listener supported
-  elUsername.addEventListener('blur', function(e) {  // On blur event
-    // NOTE: This function is checkLength() - not checkUsername()
-    checkLength(e, 5);                             // Call checkLength()
-  }, false);                                       // Capture in bubble phase
+    elUsername.addEventListener('blur', function(e) {  // On blur event
+        // NOTE: This function is checkLength() - not checkUsername()
+        checkLength(e, 5);                             // Call checkLength()
+    }, false);                                       // Capture in bubble phase
 } else {                                           // Otherwise
-  elUsername.attachEvent('onblur', function(e) {   // IE fallback onblur
-    // NOTE: This function is checkLength() - not checkUsername()
-    checkLength(e, 5);                             // Call checkLength()
-  });
-}*/
+    elUsername.attachEvent('onblur', function(e) {   // IE fallback onblur
+                                                     // NOTE: This function is checkLength() - not checkUsername()
+        checkLength(e, 5);                             // Call checkLength()
+    });
+}

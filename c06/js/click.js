ef21bafd91cdb88c5f5978aa104f5e535e78d35c
ewa1siwek <ew.siwek@gmail.com>
2018-06-29 01:26:00 +0200

    c6_click

--- c06/js/click.js
+++ c06/js/click.js
@@ -1,3 +1,21 @@
+
+var msg = '<div class="header"><a id="close" href="#">close X</a></div>';
+msg += '<div><h2>System Maintenance</h2>';
+msg += 'Our servers are being updated between 3 and 4 a.m. ';
+
+var elNote = document.createElement('div');
+elNote.setAttribute('id', 'note');
+elNote.innerHTML = msg;
+document.body.appendChild(elNote);
+function dismissNote() {
+    document.body.removeChild(elNote)
+}
+
+
+var elClose = document.getElementById('close');
+elClose.addEventListener('click', dismissNote, false);
+
+/*
 // Create the HTML for the message
 var msg = '<div class=\"header\"><a id=\"close\" href="#">close X</a></div>';
 msg += '<div><h2>System Maintenance</h2>';
@@ -14,4 +32,4 @@ function dismissNote() {                          // Declare function
 }
 
 var elClose = document.getElementById('close');   // Get the close button
-elClose.addEventListener('click', dismissNote, false);// Click close-clear note
+elClose.addEventListener('click', dismissNote, false);// Click close-clear note*/

24f07172dc7ff1fcb63df0cfb560125263dec9ed
ewa1siwek <ew.siwek@gmail.com>
2018-06-28 17:42:27 +0200

    c6_delegation-without IE

--- c06/js/event-delegation.js
+++ c06/js/event-delegation.js
@@ -1,4 +1,35 @@
 function getTarget(e) {                          // Declare function
+    return e.target;               // Get the target of event
+}
+
+function itemDone(e) {                           // Declare function
+    // Remove item from the list
+    var target,          // Declare variables
+    target = getTarget(e);                         // Get the item clicked link
+
+
+    if ( target.nodeName.toLowerCase() == "a" ) {  // If user clicked on an a element
+        elListItem = target.parentNode;              // Get its li element
+        elList = elListItem.parentNode;              // Get the ul element
+        elList.removeChild(elListItem);              // Remove list item from list
+    }
+    if ( target.nodeName.toLowerCase() == "em" ) { // If the user clicked on an em element
+        elListItem = target.parentNode.parentNode;   // Get its li element
+        elList = elListItem.parentNode;              // Get the ul element
+        elList.removeChild(elListItem);              // Remove list item from list
+    }
+     // If preventDefault() works
+        e.preventDefault();                          // Use preventDefault()
+}
+
+// Set up event listeners to call itemDone() on click
+var el = document.getElementById('shoppingList');// Get shopping list // If event listeners work
+    el.addEventListener('click', function(e) {     // Add listener on click
+        itemDone(e);                                 // It calls itemDone()
+    }, false);                                     // Use bubbling phase for flow
+/*
+
+function getTarget(e) {                          // Declare function
   if (!e) {                                      // If there is no event object
     e = window.event;                            // Use old IE event object
   }
@@ -9,6 +40,7 @@ function itemDone(e) {                           // Declare function
   // Remove item from the list
   var target, elParent, elGrandparent;           // Declare variables
   target = getTarget(e);                         // Get the item clicked link
+*/
 
   /*
   The book used the following code - but it had two shortcomings
@@ -21,6 +53,7 @@ function itemDone(e) {                           // Declare function
 
   The next 10 lines improve that example
   */
+/*
 
   if ( target.nodeName.toLowerCase() == "a" ) {  // If user clicked on an a element
     elListItem = target.parentNode;              // Get its li element
@@ -51,4 +84,4 @@ if (el.addEventListener) {                       // If event listeners work
   el.attachEvent('onclick', function(e) {        // Use old IE model: onclick
     itemDone(e);                                 // Call itemDone()
   });
-}
+}*/
